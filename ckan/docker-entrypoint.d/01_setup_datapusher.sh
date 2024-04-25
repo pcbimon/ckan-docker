1469b0c194b61465ea6ff3801da387f9dad87cc0
pcbimon <patipat.che@mahidol.edu>
2024-04-25 16:34:45 +0700

    replace datapusher to xloader

--- ckan/docker-entrypoint.d/01_setup_datapusher.sh
+++ ckan/docker-entrypoint.d/01_setup_datapusher.sh
@@ -10,3 +10,12 @@ if [[ $CKAN__PLUGINS == *"datapusher"* ]]; then
 else
    echo "Not configuring DataPusher"
 fi
+if [[ $CKAN__PLUGINS == *"xloader"* ]]; then
+   # Xloader settings have been configured in the .env file
+   # Set API token if necessary
+   if [ -z "$CKAN__XLOADER__API_TOKEN" ] ; then
+      echo "Set up ckanext.xloader.api_token in the CKAN config file"
+      ckan config-tool $CKAN_INI "ckanext.xloader.api_token=$(ckan -c $CKAN_INI user token add ckan_admin xloader | tail -n 1 | tr -d '\t')"
+   fi
+else
+   echo "Not configuring Xloader"
